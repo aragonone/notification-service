cff0e5f37540997511c41a2945484eaebb794d5c
Daniel Norman <daniel@2color.me>
2019-06-06 18:02:47 +0200

    Add migration for subscriptions table

---
+++ migrations/20190606113201_create_subscriptions.js
@@ -0,0 +1,35 @@
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('subscriptions', function(table) {
+    table
+      .increments('subscription_id')
+      .unsigned()
+      .primary()
+
+    table
+      .integer('user_id')
+      .unsigned()
+      .index()
+      .notNullable()
+    table
+      .foreign('user_id')
+      .references('user_id')
+      .inTable('users')
+
+    table.timestamps(true, true) // default to now
+    table.string('event_hash').notNullable()
+    table.string('contract_address').notNullable()
+    table.bigInteger('from_block').notNullable()
+    table.bigInteger('processed_block')
+    table.timestamp('last_poll')
+    table.jsonb('contract_abi')
+
+    table
+      .boolean('verified')
+      .notNullable()
+      .defaultTo(false)
+  })
+}
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTable('subscriptions')
+}
