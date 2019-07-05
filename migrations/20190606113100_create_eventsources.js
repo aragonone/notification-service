d881138ff032eb540291a1ba059d60b5b164d037
Daniel Norman <daniel@2color.me>
2019-07-05 16:49:12 +0200

    Add droptable to eventsources migration

--- migrations/20190606113100_create_eventsources.js
+++ migrations/20190606113100_create_eventsources.js
@@ -25,4 +25,6 @@ exports.up = function(knex, Promise) {
   })
 }
 
-exports.down = function(knex, Promise) {}
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTable('eventsources')
+}
