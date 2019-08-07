5fe335805efc4cf59ecc428c5f24f4de4419ba5e
Daniel Norman <daniel@2color.me>
2019-08-07 11:18:07 +0200

    Remove multiple environments from knex file

--- knexfile.js
+++ knexfile.js
@@ -1,12 +1,6 @@
 // Update with your config settings.
 
 module.exports = {
-  development: {
-    client: 'postgresql',
-    connection: process.env.PG_CONNECTION_STRING,
-  },
-  production: {
-    client: 'postgresql',
-    connection: process.env.PG_CONNECTION_STRING,
-  },
+  client: 'pg',
+  connection: process.env.PG_CONNECTION_STRING,
 }
